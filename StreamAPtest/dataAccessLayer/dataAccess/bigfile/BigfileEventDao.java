5131f510db8b019407e4f5270c525afbaab87d06
Morteza Neishaboori <morteza.neishaboori@gmail.com>
2016-06-12 23:05:28 +0200

    .

--- StreamAPtest/dataAccessLayer/dataAccess/bigfile/BigfileEventDao.java
+++ StreamAPtest/dataAccessLayer/dataAccess/bigfile/BigfileEventDao.java
@@ -14,6 +14,30 @@ public class BigfileEventDao implements IEventDao {
 	public void startLoading() {
 		// TODO Auto-generated method stub
 		
+		
+	/*	FileInputStream inputStream = null;
+		Scanner sc = null;
+		try {
+		    inputStream = new FileInputStream(path);
+		    sc = new Scanner(inputStream, "UTF-8");
+		    while (sc.hasNextLine()) {
+		        String line = sc.nextLine();
+		        // System.out.println(line);
+		    }
+		    // note that Scanner suppresses exceptions
+		    if (sc.ioException() != null) {
+		        throw sc.ioException();
+		    }
+		} finally {
+		    if (inputStream != null) {
+		        inputStream.close();
+		    }
+		    if (sc != null) {
+		        sc.close();
+		    }
+		}*/
+		
 	}
+	
 
 }
