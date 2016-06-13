422d1d0b2084ffcd7b52dead3f2c952f0675330b
Morteza Neishaboori <morteza.neishaboori@gmail.com>
2016-06-13 15:35:25 +0200

    .

--- StreamAPtest/businessLogicLayer/businessLogic/Logic.java
+++ StreamAPtest/businessLogicLayer/businessLogic/Logic.java
@@ -1,5 +1,12 @@
 package businessLogic;
 
+
+/**
+ * This class collects the entities from the data access layer queue
+ * and distributes them properly for the created pipline and registered processors
+ * @author Morteza Neishaboori
+ *
+ */
 public class Logic {
 
 }
